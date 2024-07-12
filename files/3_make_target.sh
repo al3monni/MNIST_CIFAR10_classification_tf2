91189a15c12b70dd6ffca3e2d50b34040beafb6c
al3monni <monni.alex4@gmail.com>
2024-07-12 16:11:59 +0200

    Update 3_make_target.sh

--- files/3_make_target.sh
+++ files/3_make_target.sh
@@ -31,20 +31,20 @@ rm -rf ${TARGET_ZCU102}
 mkdir -p ${TARGET_ZCU102}/model_dir
 
 
-# copy application to TARGET_KV260 folder
+# copy application to TARGET_KV260 folder, comment this function if you don't need the kria support to dont't waste time
 cp ${APP}/*.py ${TARGET_KV260}
 echo " Copied application to TARGET_KV260 folder"
 
-# copy application to TARGET_ZCU102 folder
+# copy application to TARGET_ZCU102 folder, comment this function if you don't need the zcu support to dont't waste time
 cp ${APP}/*.py ${TARGET_ZCU102}
 echo " Copied application to TARGET_ZCU102 folder"
 
 
-# copy xmodel to TARGET_KV260 folder
+# copy xmodel to TARGET_KV260 folder, comment this function if you don't need the kria support to dont't waste time
 cp ${COMPILE_KV260}/${NET_NAME}.xmodel ${TARGET_KV260}/model_dir/.
 echo " Copied xmodel file(s) to TARGET_KV260 folder"
 
-# copy xmodel to TARGET_ZCU102 folder
+# copy xmodel to TARGET_ZCU102 folder, comment this function if you don't need the zcu support to dont't waste time
 cp ${COMPILE_ZCU102}/${NET_NAME}.xmodel ${TARGET_ZCU102}/model_dir/.
 echo " Copied xmodel file(s) to TARGET_ZCU102 folder"
 
@@ -55,7 +55,7 @@ mkdir -p ${TARGET_KV260}/images
 # create image files and copy to target folder
 mkdir -p ${TARGET_ZCU102}/images
 
-# generate test image for inference
+# generate test image for inference, comment this function if you don't need the kria support to dont't waste time
 python generate_images.py  \
     --dataset=mnist \
     --image_dir=${TARGET_KV260}/images \
@@ -64,7 +64,7 @@ python generate_images.py  \
 
 echo "  Copied images to TARGET_KV260 folder"
 
-# generate test image for inference
+# generate test image for inference, comment this function if you don't need the zcu support to dont't waste time
 python generate_images.py  \
     --dataset=mnist \
     --image_dir=${TARGET_ZCU102}/images \
